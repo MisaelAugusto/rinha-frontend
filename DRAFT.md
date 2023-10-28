6d62db0bdbecac725542216141447cb71230ac93
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2023-10-28 20:55:08 -0300

    added first script to render objects of objects

---
+++ DRAFT.md
@@ -0,0 +1,79 @@
+key: string -> value: object | array | number | string | boolean | null | undefined
+
+    <div id="json-container">
+      <header>&#123;</header>
+
+      <div id="glossary" style="margin-left:1rem;">
+        <header>glossary: &#123;</header>
+
+        <div style="margin-left:1rem;">
+          <p>empty: ""</p>
+          <p>p2: "p2"</p>
+          <p>title: "example glossary"</p>
+        </div>
+
+        <footer>&#125;</footer>
+      </div>
+
+      <div id="glossary" style="margin-left:1rem;">
+        <header>glossary: &#123;</header>
+
+        <div style="margin-left:1rem;">
+          <p>empty: ""</p>
+          <p>p2: "p2"</p>
+          <p>title: "example glossary"</p>
+        </div>
+
+        <footer>&#125;</footer>
+      </div>
+
+      <div id="glossary" style="margin-left:1rem;">
+        <header>glossary: &#91;</header>
+
+        <div style="margin-left:1rem;">
+          <p>0: ""</p>
+          <p>1: "p2"</p>
+          <p>2: "example glossary"</p>
+        </div>
+
+        <footer>&#93;</footer>
+      </div>
+
+      <div id="glossary" style="margin-left:1rem;">
+        <header>glossary: &#91;</header>
+
+        <div style="margin-left:1rem;">
+          <header>0:</header>
+
+          <div style="margin-left:1rem;">
+            <p>empty: ""</p>
+            <p>p2: "p2"</p>
+            <p>title: "example glossary"</p>
+          </div>
+        </div>
+
+        <div style="margin-left:1rem;">
+          <header>1:</header>
+
+          <div style="margin-left:1rem;">
+            <p>empty: ""</p>
+            <p>p2: "p2"</p>
+            <div id="glossary">
+              <header>glossary: &#91;</header>
+
+              <div style="margin-left:1rem;">
+                <p>0: ""</p>
+                <p>1: "p2"</p>
+                <p>2: "example glossary"</p>
+              </div>
+
+              <footer>&#93;</footer>
+            </div>
+          </div>
+        </div>
+
+        <footer>&#93;</footer>
+      </div>
+
+      <footer>&#125;</footer>
+    </div>
---
+++ script.js
@@ -0,0 +1,118 @@
+// EMPTY JSON CONTAINER WHEN UPLOADING OTHER JSON
+// RENDER ARRAY
+// STYLE BASED ON TYPES
+// STYLE JSON CONTAINER
+// STYLE PROPS HIERARCHY
+// IMPLEMENT PAGINATIO
+
+const HEADER_OBJECT_ELEMENT = document.createElement('header');
+HEADER_OBJECT_ELEMENT.textContent = '{';
+
+const FOOTER_OBJECT_ELEMENT = document.createElement('footer');
+FOOTER_OBJECT_ELEMENT.textContent = '}';
+
+const jsonContainerElement =  document.createElement('div');
+
+const fileNameElement = document.getElementById('file-name');
+
+const getValueElement = (value) => {
+  const propertyValuesValueElement = document.createElement('span');
+  propertyValuesValueElement.classList.add(typeof value);
+  
+  let formattedValue = value;
+  
+  if (typeof value === 'string') {
+    formattedValue = `"${value}"`
+  }
+  
+  propertyValuesValueElement.textContent = formattedValue;
+
+  return propertyValuesValueElement;
+};
+
+const getChildProps = (prop, parentElement) => {
+  const childPropsElement = document.createElement('div');
+  childPropsElement.classList.add('indentation');
+
+  Object.entries(prop).forEach(([key, value]) => {
+    const childPropKeyElement = document.createElement('p');
+    childPropKeyElement.textContent = `${key}: `;
+
+    if (typeof value === 'object') {
+      renderProp(key, value, parentElement);
+    } else {
+      const childPropValueElement = getValueElement(value);
+      
+      childPropKeyElement.appendChild(childPropValueElement);
+      
+      childPropsElement.append(childPropKeyElement);
+    }
+  })
+
+  return childPropsElement;
+};
+
+const renderProp = (key, value, parentElement) => {
+  const propertyDiv = document.createElement('div');
+  propertyDiv.classList.add('indentation');
+
+  const propertyHeader = document.createElement('header');
+  propertyHeader.textContent = `${key}: `;
+
+  propertyDiv.appendChild(propertyHeader);
+
+  if (typeof value === 'object') {
+    propertyHeader.textContent += '{';
+
+    const childPropsElement = getChildProps(value, propertyDiv);
+    
+    propertyDiv.appendChild(childPropsElement);
+
+    const footerElement = FOOTER_OBJECT_ELEMENT.cloneNode(true);
+    propertyDiv.appendChild(footerElement);
+  }
+
+  parentElement.appendChild(propertyDiv);
+};
+
+const renderJson = (file, object) => {
+  fileNameElement.textContent = file.name;
+
+  jsonContainerElement.appendChild(HEADER_OBJECT_ELEMENT);
+
+  Object.entries(object).forEach(([key, value])=> {
+    renderProp(key, value, jsonContainerElement);
+  });
+
+  const footerElement = FOOTER_OBJECT_ELEMENT.cloneNode(true);
+  jsonContainerElement.appendChild(footerElement);
+
+  const [mainElement] = document.getElementsByTagName('main');
+  mainElement.appendChild(jsonContainerElement);
+};
+
+const jsonInput = document.getElementById('json-input');
+jsonInput.onchange = (e) => {
+  const file = e.target.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    const errorMessageElement = document.getElementById('error-message');
+
+    reader.onload = () => {
+      try {
+        const object = JSON.parse(reader.result);
+
+        renderJson(file, object);
+
+        errorMessageElement.hidden = true;
+      } catch {
+        errorMessageElement.removeAttribute('hidden');
+        fileNameElement.textContent = '';
+      }
+    };
+    
+    reader.readAsText(file);
+  }
+}
